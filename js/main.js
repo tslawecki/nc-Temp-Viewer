c0b0816dfdee0455f3f39295e9fd73569cae8085
tslawecki <tad@limno.com>
2017-09-15 14:22:17 -0400

    Broad outline of steps added to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 /**
  * Created by tslawecki on 9/15/2017.
  */
+
+
+//
+// OUTLINE:
+//
+// ----- (Select lake)
+//
+// ----- (Set resolution)
+//
+// ----- Read latest time
+//
+// ----- Read results
+//
+// ----- Translate from sigma coordinate to actual depths and interpolate temperatures
+//
+// ----- Display
+//
+// ----- Respond to UI
+//
