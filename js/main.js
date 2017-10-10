95b1e7b32c3badc8a5065b62d09ae0d3bac42f02
tslawecki <tad@limno.com>
2017-10-09 23:41:10 -0400

    Parse temperature section

--- js/main.js
+++ js/main.js
@@ -35,7 +35,6 @@ function textFileToArray(filename) {
 }
 
 const depthsStart = 5;
-const tempsStart = 96;
 var depths = [];
 var temperatures = [];
 
@@ -45,15 +44,43 @@ function getResults(url) {
 
     var responseText = textFileToArray('data/Lake_Erie_-_Nowcast_-_3D_-_Current_Year_best.ncd.txt');
 
-    var strDepthsSplit;
-    strDepthsSplit = responseText[depthsStart].replace(/]/g, '[').split("[");
+    var strDepthsSplit = responseText[depthsStart].replace(/]/g, '[').split("[");
     if (strDepthsSplit[0] != "depth") {
         alert("Expected 'depth[][]', found '" + responseText[depthsStart] + "'!");
     }
     else {
+
+        // Read depths
+
         for (i = 0; i < strDepthsSplit[1]; i++) {
             depths.push(responseText[depthsStart + 1 + i].split(",").slice(1, strDepthsSplit[3]));
         }
+
+        // Check for temperatures
+
+        var tempsStart = depthsStart + parseInt(strDepthsSplit[1]) + 2;
+        var strTempsSplit = responseText[tempsStart].replace(/]/g, '[').split("[");
+        if (strTempsSplit[0] != "temp") {
+            alert("Expected 'temp[][]', found '" + responseText[tempsStart] + "'!");
+        }
+        else {
+
+            // Read temperatures
+
+            for (i = 0; i < strTempsSplit[3]; i++) {
+                var layer = [];
+                for (j = 0; j < strTempsSplit[5]; j++) {
+                    tempsStart++;
+                    layer.push(responseText[tempsStart].split(",").slice(1, strTempsSplit[7]));
+                }
+                temperatures.push(layer);
+            }
+
+            // Build (1) depth to critical temperature
+
+            // Build (2) 3D regularly spaced array
+        }
+
     }
 
 
